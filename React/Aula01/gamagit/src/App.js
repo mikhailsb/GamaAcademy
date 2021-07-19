0e62541e805543181bb2744c09f1c06f2dffe943
Mikhail Bispo <anaomsb@hotmail.com>
2021-07-19 19:58:22 -0300

    Aula 4 de React

--- React/Aula01/gamagit/src/App.js
+++ React/Aula01/gamagit/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App(props) {
+  const [usuario, setUsuario ] = React.useState('');
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response));
+  }
+
   return (
     <>
-      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário"/>
-      <button type="button">Pesquisar</button>
+      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
     </>
   );
 }
