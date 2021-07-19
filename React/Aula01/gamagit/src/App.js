d22520de1dc83380070f9ae994555a9edd7b3a8f
Mikhail Bispo <anaomsb@hotmail.com>
2021-07-19 17:03:45 -0300

    Aula 1 a 3 de React

---
+++ React/Aula01/gamagit/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <>
+      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário"/>
+      <button type="button">Pesquisar</button>
+    </>
+  );
+}
+
+export default App;
